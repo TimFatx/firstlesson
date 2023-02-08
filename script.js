ed923b9fd59804faf660e00e87a6e81e068180c8
Timur <fatxievt@gmail.com>
2023-02-08 21:04:18 +0500

    first edits

---
+++ script.js
@@ -0,0 +1,54 @@
+// alert('Hello');
+// console.log(2);
+
+
+// let number = 5;
+// const leftBorder = 1;
+
+// leftBorder = 4+number;
+
+// console.log (leftBorder);
+
+// const result = confirm("Are you here?");
+// console.log(result);
+
+// const answer = prompt("Логин", "");
+// console.log(answer + 5);    будет строка
+
+// const answer = +prompt("Логин", "");
+// console.log(answer + 5);      будет число, засчет плюса перед promt
+
+
+// const answers = [];
+
+// answers[0] = prompt('Логин', '');
+// answers[1] = prompt('Пароль', '');
+
+// document.write(answers);
+
+// console.log(typeof(answers));
+
+
+// const category = 'toys';
+
+// console.log(`https://rome.com/${category}/5`);
+
+
+
+
+// const yourName = [];
+
+// yourName[0] = prompt("Ваше имя", "");
+// yourName[1] = prompt("Ваша фамилия", "");
+
+// alert(`Привет, ${yourName}`);
+
+// let oneDay = 10,
+//     twoDay = 10;
+//     ++oneDay;
+//     --twoDay;
+// console.log(oneDay);
+// console.log(twoDay);
+
+// console.log( 5 + 5 == 10);
+
